c26107b9087988d5610aa01a5ba6730bc297dffa
Samuel dos Santos <samuelsantosp10@gmail.com>
2024-10-10 14:54:20 -0300

    Exercicío 02 da aula 01

---
+++ exemplo_02.py
@@ -0,0 +1,7 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+print("O número da soma dos valores digitados é: " + str(int(input("Digite o primeiro numero")) + int(input("Digite o segundo numero"))))
+
+# print("O número da soma dos valores digitados é: " + str(int(input("Digite o primeiro numero"))))
+
+# print(int(input("Digite o primeiro numero")) + int(input("Digite o segundo numero")))
